dd2eac8dff0d30c5087afb21cd7ce802e09d021a
Kostas Spyropoulos <inigo.aldana@gmail.com>
2010-09-14 19:39:30 -0400

    measuring different deck

--- src/com/ichi2/anki/tests/PerformanceTest.java
+++ src/com/ichi2/anki/tests/PerformanceTest.java
@@ -13,6 +13,7 @@ import android.widget.SimpleAdapter;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.util.Log;
+import android.test.suitebuilder.annotation.LargeTest;
 
 import java.util.ArrayList;
 import java.lang.System;
@@ -20,7 +21,8 @@ import java.lang.System;
 public class PerformanceTest extends ActivityInstrumentationTestCase2<StudyOptions> {
 
 	private static final String TAG = "AnkiDroidTest";
-	private static final String DECK = "slow-fixed";
+	private static final String DECK = "slow-vacuum";
+	private static final int ANSCARDS = 200;
 
 	private Solo solo;
 
@@ -38,20 +40,38 @@ public class PerformanceTest extends ActivityInstrumentationTestCase2<StudyOptio
 		ArrayList<ListView> deck_picker_list = solo.getCurrentListViews();
 		assertNotNull(deck_picker_list);
 		ListView lv = deck_picker_list.get(0);
-		ProgressBar pg;
+		View pg;
+		View tv;
+		View v;
 		boolean allDone = false;
+		boolean notFound = true;
+		int decki = 0;
 		while (!allDone) {
 			allDone = true;
 			for (int i = 0; i < lv.getChildCount(); i++) {
-				pg = (ProgressBar)(((LinearLayout)(((LinearLayout)(lv.getChildAt(i))).getChildAt(1))).getChildAt(2));
-				if (pg.getVisibility() == View.VISIBLE) {
+				v = lv.getChildAt(i);
+				assertTrue(v instanceof LinearLayout);
+				tv = ((LinearLayout)v).getChildAt(0);
+				assertTrue(tv instanceof TextView);
+				v = ((LinearLayout)v).getChildAt(1);
+				assertTrue(v instanceof LinearLayout);
+				pg = ((LinearLayout)v).getChildAt(2);
+				assertTrue(pg instanceof ProgressBar);
+				if (notFound && ((TextView)tv).getText().toString().equals(deck)) {
+					notFound = false;
+					decki = i;
+					Log.i(TAG, "Found requested deck in position " + decki);
+				}
+				if (((ProgressBar)pg).getVisibility() == View.VISIBLE) {
 					allDone = false;
 				}
 			}
 		}
-		assertTrue(solo.waitForText(deck));
-		solo.clickOnText(deck);
-		assertTrue(solo.waitForActivity("StudyOptions", 60000));
+		//assertTrue(solo.waitForText(deck));
+		//solo.clickOnText(deck);
+
+		solo.clickInList(decki);
+		assertTrue(solo.waitForActivity("StudyOptions", 10000));
 
 	}
 
@@ -66,13 +86,13 @@ public class PerformanceTest extends ActivityInstrumentationTestCase2<StudyOptio
 	@LargeTest
 	public void test1() throws Exception {
 		helperPickDeck(DECK);
-		Log.i(TAG, "Start reviewing deck " + DECK + " - 100 iterations.");
+		Log.i(TAG, "Start reviewing deck " + DECK + " - " + ANSCARDS + " iterations.");
 		solo.clickOnButton("Start Reviewing");
 		assertTrue(solo.waitForActivity("Reviewer", 5));
 		Reviewer rev = (Reviewer)(solo.getCurrentActivity());
 		long tottime = 0;
 
-		for (int j = 0; j < 100; j++) {
+		for (int j = 0; j < ANSCARDS; j++) {
 			assertTrue(solo.waitForText("Show Answer"));
 			solo.clickOnButton("Show Answer");
 			assertTrue(solo.waitForText("Easy", 1, 1));
@@ -82,9 +102,12 @@ public class PerformanceTest extends ActivityInstrumentationTestCase2<StudyOptio
 			assertTrue(solo.waitForDialogToClose(15000));
 
 			tottime += rev.lastTime;
-			Log.i(TAG, "Loop: " + j + " last: " + (System.currentTimeMillis() - start) + " real last: " + rev.lastTime +
-					" avg: " + tottime/(j+1) + " real avg: " + rev.avgTime);
+			Log.d(TAG, "Loop: " + j + " last: " + (System.currentTimeMillis() - start) + " real last: " + rev.lastTime + " avg: " + tottime/(j+1) + " real avg: " + rev.avgTime);
+			if (j % 25 == 0) {
+				Log.i(TAG, "Loop: " + j + " last: " + (System.currentTimeMillis() - start) + " real last: " + rev.lastTime + " avg: " + tottime/(j+1) + " real avg: " + rev.avgTime);
+			}
 		}
+		Log.i(TAG, "Average: " + (tottime/((double)ANSCARDS)) + " app avg: " + rev.avgTime);
 	}
 
 	@Override
