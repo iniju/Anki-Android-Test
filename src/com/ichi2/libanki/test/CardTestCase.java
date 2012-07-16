78848bf52993656d7671b93fcea92dd6a4dac7b8
iniju <inigo.aldana@gmail.com>
2012-07-17 00:26:28 +0100

    libanki as of 16-07-2012

--- src/com/ichi2/libanki/test/CardTestCase.java
+++ src/com/ichi2/libanki/test/CardTestCase.java
@@ -52,13 +52,11 @@ public class CardTestCase extends InstrumentationTestCase {
 		long cid = f.cards().get(0).getId();
 		deck.reset();
 		deck.getSched().answerCard(deck.getSched().getCard(), 2);
-		assertTrue(deck.getDb().queryScalar("SELECT count() FROM revlog") == 1);
 		deck.remCards(new long[]{cid});
 		assertTrue(deck.cardCount() == 0);
 		assertTrue(deck.noteCount() == 0);
 		assertTrue(deck.getDb().queryScalar("SELECT count() FROM notes") == 0);
 		assertTrue(deck.getDb().queryScalar("SELECT count() FROM cards") == 0);
-		assertTrue(deck.getDb().queryScalar("SELECT count() FROM revlog") == 0);
 		assertTrue(deck.getDb().queryScalar("SELECT count() FROM graves") == 2);
 	}
 	
