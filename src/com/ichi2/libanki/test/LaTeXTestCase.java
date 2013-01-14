5a25f0a00d45a667e812335bfeb76e07e3fe0a8c
iniju <inigo.aldana@gmail.com>
2013-01-14 14:15:35 +0100

    formatting - tabs to spaces

--- src/com/ichi2/libanki/test/LaTeXTestCase.java
+++ src/com/ichi2/libanki/test/LaTeXTestCase.java
@@ -8,32 +8,32 @@ import com.ichi2.libanki.Collection;
 import com.ichi2.libanki.Note;
 
 public class LaTeXTestCase extends InstrumentationTestCase {
-	public LaTeXTestCase(String name) {
-		setName(name);
-	}
+    public LaTeXTestCase(String name) {
+        setName(name);
+    }
 
-	@MediumTest
-	public void test_latex() {
-		Collection d = Shared.getEmptyDeck(getInstrumentation().getContext());
-		// skipping the image generation and just checking the links in this test
-		Note f = d.newNote();
-		f.setitem("Front", "[latex]hello[/latex]");
-		d.addNote(f);
-		assertTrue(f.cards().get(0).getQuestion(false).contains(".png"));
-		// adding a different note should create different link
-		Card oldcard = f.cards().get(0);
-		f = d.newNote();
-		f.setitem("Front", "[latex]world[/latex]");
-		d.addNote(f);
-		assertTrue(f.cards().get(0).getQuestion(false).contains(".png"));
-		MoreAsserts.assertNotEqual(f.cards().get(0).getQuestion(false), oldcard.getQuestion(false));
-		// another note with the same media should reuse
-		oldcard = f.cards().get(0);
-		f = d.newNote();
-		f.setitem("Front", "[latex]world[/latex]");
-		d.addNote(f);
-		assertEquals(f.cards().get(0).getQuestion(false), oldcard.getQuestion(false));
-	}
+    @MediumTest
+    public void test_latex() {
+        Collection d = Shared.getEmptyDeck(getInstrumentation().getContext());
+        // skipping the image generation and just checking the links in this test
+        Note f = d.newNote();
+        f.setitem("Front", "[latex]hello[/latex]");
+        d.addNote(f);
+        assertTrue(f.cards().get(0).getQuestion(false).contains(".png"));
+        // adding a different note should create different link
+        Card oldcard = f.cards().get(0);
+        f = d.newNote();
+        f.setitem("Front", "[latex]world[/latex]");
+        d.addNote(f);
+        assertTrue(f.cards().get(0).getQuestion(false).contains(".png"));
+        MoreAsserts.assertNotEqual(f.cards().get(0).getQuestion(false), oldcard.getQuestion(false));
+        // another note with the same media should reuse
+        oldcard = f.cards().get(0);
+        f = d.newNote();
+        f.setitem("Front", "[latex]world[/latex]");
+        d.addNote(f);
+        assertEquals(f.cards().get(0).getQuestion(false), oldcard.getQuestion(false));
+    }
 
     @MediumTest
     public void test_more_latex() {
